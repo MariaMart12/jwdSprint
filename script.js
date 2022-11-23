ade796799cc7966c539f8730f17060ab92b9a1ea
shahanJP <113164178+shahanJP@users.noreply.github.com>
2022-11-23 19:04:40 +1100

    form validation completed

--- script.js
+++ script.js
@@ -2,69 +2,87 @@
 
 //Variable Target///
 
+const form = document.querySelector("#form");
 const inputValue = document.getElementById("InputValue");
 const assignValue = document.getElementById("assignValue");
 const statusValue = document.getElementById("statusValue");
 const dateValue = document.getElementById("dateValue");
 const descValue = document.getElementById("descValue");
 
-const submitBtn = document.getElementById("submitBtn")
+form.addEventListener("submit", (event) => {
+  validateForm();
+  if (isFormValid() == true) {
+    form.submit();
+  } else {
+    event.preventDefault();
+  }
+});
 
-submitBtn.addEventListener("click", validateForm)
+function isFormValid() {
+  const inputContainers = document.querySelectorAll(".input-group");
+  let result = true;
+  inputContainers.forEach((container) => {
+    if (container.classList.contains("error")) {
+      result = false;
+    }
+  });
+  return result;
+}
 
-const validateForm = (event)=>{
-     event.preventDefault();
-     //Task Name
-     if(inputValue.value.trim() === ""){
-        setError(inputValue, "Please Enter Your Name")
-     }else{
-          setSuccess(inputValue);
-     }
-     //Assign To
-     if(assignValue.value.trim() === ""){
-          setError(assignValue, "Please Choose Team Member")
-       }else{
-          setSuccess(assignValue);
-     }
-     //Status
-     if(statusValue.value.trim() === ""){
-          setError(statusValue, "Please Choose Your Status")
-       }else{
-          setSuccess(statusValue);
-     }
+function validateForm() {
+  //Task Name
+  if (inputValue.value.trim() === "") {
+    setError(inputValue, "*Please Enter Your Name");
+  } else {
+    setSuccess(inputValue);
+  }
+  //Assign To
+  if (assignValue.value === "") {
+    setError(assignValue, "*Please Choose Team Member");
+  } else {
+    setSuccess(assignValue);
+  }
+  //Status
+  if (statusValue.value === "") {
+    setError(statusValue, "*Please Choose Your Status");
+  } else {
+    setSuccess(statusValue);
+  }
 
-       //date
-       if(dateValue.name.trim() === ""){
-          setError(dateValue, "Please Choose Your Due Date")
-       }else{
-          setSuccess(dateValue);
-     }
-       //Description
-       if(descValue.value.trim().length < 10 || descValue.value.trim()>200){
-          setError(descValue, "Description Minimum 10 and Maximum 200 characters long")
-       }
-       else{
-       if(descValue.value.trim().length < 10 || descValue.value.trim()>200){
-       }else{
-          setSuccess(descValue);
-       }
-       }
-};
+  //date
+  if (dateValue.value == "" ) {
+    setError(dateValue, "*Please Choose Your Due Date");
+  } else {
+    setSuccess(dateValue);
+  }
+  //Description
+  if (descValue.value.trim().length < 10 || descValue.value.trim() > 200) {
+    setError(
+      descValue,
+      "*Description Minimum 10 and Maximum 200 characters long"
+    );
+  } else {
+    if (descValue.value.trim().length < 10 || descValue.value.trim() > 200) {
+    } else {
+      setSuccess(descValue);
+    }
+  }
+}
 
 function setError(element, errorMessage) {
-     const parent = element.parentElement;
-     if (parent.classList.contains("success")) {
-       parent.classList.remove("success");
-     }
-     parent.classList.add("error");
-     const paragraph = parent.querySelector("p");
-     paragraph.textContent = errorMessage;
-   }
-   function setSuccess(element) {
-     const parent = element.parentElement;
-     if (parent.classList.contains("error")) {
-       parent.classList.remove("error");
-     }
-     parent.classList.add("success");
-   }
-   
+  const parent = element.parentElement;
+  if (parent.classList.contains("success")) {
+    parent.classList.remove("success");
+  }
+  parent.classList.add("error");
+  const paragraph = parent.querySelector("p");
+  paragraph.textContent = errorMessage;
+}
+
+function setSuccess(element) {
+  const parent = element.parentElement;
+  if (parent.classList.contains("error")) {
+    parent.classList.remove("error");
+  }
+  parent.classList.add("success");
+}
